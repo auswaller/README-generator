f03a8705eb1b37800d78bbc6b45b3b9feeb3e446
Austin W <auswaller@gmail.com>
2023-05-15 16:21:42 -0400

    added more questions, started call to generateMarkdown script

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require('./utils/generateMarkdown');
 
 const questions = [
     "Please enter the project title",
@@ -7,51 +8,70 @@ const questions = [
     "Please enter the installation instructions",
     "Please enter the usage information",
     "Please enter the contribution guidelines",
-    "Please enter the test instructions"
+    "Please enter the test instructions",
+    "Please select the license for the project",
+    "Please enter your GitHub username",
+    "Please enter your email address"
 ];
 
 function writeToFile(fileName, data) {
-    fs.writeFile(`${fileName}.txt`,data, 
-    (err) => err ? console.error(err) : console.log('Successfully wrote file!'));
+    fs.writeFile(`${fileName}.txt`, data,
+        (err) => err ? console.error(err) : console.log('Successfully wrote file!'));
 }
 
 function init() {
     inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: questions[0],
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: questions[1],
-      name: 'description',
-    },
-    {
-      type: 'input',
-      message: questions[2],
-      name: 'installation',
-    },
-    {
-        type: 'input',
-        message: questions[3],
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: questions[4],
-        name: 'contributions',
-      },
-      {
-        type: 'input',
-        message: questions[5],
-        name: 'test',
-      },
-  ])
-  .then((response) => {
-    
-  });
+        .prompt([
+            {
+                type: 'input',
+                message: questions[0],
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: questions[1],
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: questions[2],
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: questions[3],
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: questions[4],
+                name: 'contributions',
+            },
+            {
+                type: 'input',
+                message: questions[5],
+                name: 'test',
+            },
+            {
+                type: 'list',
+                message: questions[6],
+                name: 'license',
+                choices: ['MIT', 'Apache License 2.0', 'GNU GPLv3', 'ISC License'],
+            },
+            {
+                type: 'input',
+                message: questions[7],
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: questions[8],
+                name: 'email',
+            },
+        ])
+        .then((response) => {
+            writeToFile(response.name,generateMarkdown(response));         
+        });
 }
 
 init();
